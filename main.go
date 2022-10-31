51d48e3ea1860389df4feb9a2a7937bae018947b
Anurag Tangri <anuragtangri321@gmail.com>
2022-10-31 19:32:35 -0400

    adding addFood functionality

--- main.go
+++ main.go
@@ -5,6 +5,22 @@ import (
 	"os"
 )
 
+type Food struct {
+	name        string
+	calories    int
+	description string
+}
+
+func addFood() {
+	fmt.Println("For Adding Food Press 1")
+	fmt.Println("For Deleting Food Press 2")
+	var val int
+	_, err := fmt.Scanf("%d", &val)
+	if err != nil {
+		fmt.Println("Error while entering food option value")
+	}
+}
+
 func main() {
 
 	fmt.Println("Welcome to Calorie Tracker")
@@ -16,7 +32,7 @@ func main() {
 	var caseVal int
 	_, err := fmt.Scanf("%d", &caseVal)
 	if err != nil {
-		fmt.Println("Error while entering case value")
+		fmt.Println("Error while entering option value")
 	}
 	if caseVal < 1 || caseVal > 3 {
 		fmt.Println("Invalid case value")
@@ -25,6 +41,7 @@ func main() {
 	switch {
 	case caseVal == 1:
 		fmt.Println("Calling functionality for Adding/Removing Food")
+		addFood()
 	case caseVal == 2:
 		fmt.Println("Calling functionality for Calculating Total Calories")
 	case caseVal == 3:
