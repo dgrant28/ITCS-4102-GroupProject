ecf4730520f2f8cc2b68c1940f4373904d51b4c4
Anurag Tangri <anuragtangri321@gmail.com>
2022-10-31 19:14:09 -0400

    adding switch case for options

--- main.go
+++ main.go
@@ -1,62 +1,26 @@
 package main
 
-import (
-	"html/template"
-	"log"
-	"net/http"
-	"time"
-)
-
-type variables struct {
-	date string
-	time string
-}
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", addPersonal)
-	log.Fatal(http.ListenAndServe(":3000", nil))
-}
 
-func addPersonal(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Welcome to Calorie Tracker")
 
-	now := time.Now()
-	HomePageVars := variables{
-		date: now.Format("02-01-2006"),
-		time: now.Format("15:04:05"),
+	fmt.Println("For Adding/Removing Food Press 1")
+	fmt.Println("For Calculate Total Calories Press 2")
+	fmt.Println("For Adding Personal Physical Information Press 3")
+
+	var caseVal int
+	_, err := fmt.Scanf("%d", &caseVal)
+	if err != nil {
+		fmt.Println("case value entered is wrong")
 	}
-
-	t, error := template.ParseFiles("./views/index.html")
-	if error != nil {
-		log.Print("template parsing error: ", error)
+	switch {
+	case caseVal == 1:
+		fmt.Println("Calling functionality for Adding/Removing Food")
+	case caseVal == 2:
+		fmt.Println("Calling functionality for Calculating Total Calories")
+	case caseVal == 3:
+		fmt.Println("Calling functionality for Adding Personal Physical Information")
 	}
-	error = t.Execute(w, HomePageVars)
-	if error != nil {
-		log.Print("template executing error: ", error)
-	}
-}
-
-var totalCalories int
-
-func addFood(calories int) int {
-	totalCalories = calories + totalCalories
-	return totalCalories
-}
-
-func removeFood(calories int) int {
-	totalCalories = calories - totalCalories
-	return totalCalories
-}
-
-func calcTotal(calories int) {
-
-}
-
-func addPhysical(height string, weight string) {
-	var strArray [2]string
-	strArray[0] = height
-	strArray[1] = weight
-}
-
-func logExercise(exercise string) string {
-	return exercise
 }
